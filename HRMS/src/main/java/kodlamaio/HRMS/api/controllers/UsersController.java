6b0f68e44b42aad68e2adec856f70384618a7e60
Kadri Sertan Basyigit <ksbasyigit@gmail.com>
2021-05-15 18:06:53 +0300

    Get Hata çözümlenemdi.

--- HRMS/src/main/java/kodlamaio/HRMS/api/controllers/UsersController.java
+++ HRMS/src/main/java/kodlamaio/HRMS/api/controllers/UsersController.java
@@ -2,8 +2,11 @@ package kodlamaio.HRMS.api.controllers;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,8 +38,15 @@ public class UsersController {
 		return userService.add(user);
 	}
 
-	@PostMapping("/get")
-	public User get(int id) {
+	@GetMapping("/get/{id}")
+	public User get(@PathVariable int id) {
 		return userService.get(id);
 	}
+	//HATA: "Type definition error: [simple type, class org.hibernate.proxy.pojo.bytebuddy.
+	//       ByteBuddyInterceptor]; nested exception is com.fasterxml.jackson.databind.exc.InvalidDefinitionException: 
+	//       No serializer found for class org.hibernate.proxy.pojo.bytebuddy.
+	//       ByteBuddyInterceptor and no properties discovered to create BeanSerializer (to avoid exception, disable SerializationFeature.
+	//       FAIL_ON_EMPTY_BEANS) (through reference chain: kodlamaio.HRMS.entities.concretes.
+	//       User$HibernateProxy$X62IzM7u[\"hibernateLazyInitializer\"])",
+	// Hata çözümlenemedi.
 }
